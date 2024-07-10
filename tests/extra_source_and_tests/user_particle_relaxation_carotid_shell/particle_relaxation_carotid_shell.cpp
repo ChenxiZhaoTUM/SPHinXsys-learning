b07a6f26d3eecfa341b170fbab4d1ee817c2dc90
zhaocx <zhaocx@stu.xjtu.edu.cn>
2024-07-10 15:31:03 +0200

    add carotid wall as shell model

--- tests/extra_source_and_tests/user_particle_relaxation_carotid_shell/particle_relaxation_carotid_shell.cpp
+++ tests/extra_source_and_tests/user_particle_relaxation_carotid_shell/particle_relaxation_carotid_shell.cpp
@@ -26,6 +26,8 @@ Vec3d domain_upper_bound(12.0 * scaling, 10.0 * scaling, 23.5 * scaling);
 //----------------------------------------------------------------------
 BoundingBox system_domain_bounds(domain_lower_bound, domain_upper_bound);
 Real dp_0 = 0.1 * scaling;
+Real thickness = 1.0 * dp_0;
+Real level_set_refinement_ratio = dp_0 / (0.1 * thickness);
 //----------------------------------------------------------------------
 //	define the imported model.
 //----------------------------------------------------------------------
@@ -34,7 +36,7 @@ class SolidBodyFromMesh : public ComplexShape
   public:
     explicit SolidBodyFromMesh(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        add<ExtrudeShape<TriangleMeshShapeSTL>>(4.0 * dp_0, full_path_to_file, translation, scaling);
+        add<ExtrudeShape<TriangleMeshShapeSTL>>(thickness, full_path_to_file, translation, scaling);
         subtract<TriangleMeshShapeSTL>(full_path_to_file, translation, scaling);
     }
 };
@@ -74,15 +76,15 @@ RotationResult RotationCalculator(Vecd target_normal, Vecd standard_direction)
 }
 
 // inlet R=2.9293, (1.5611, 5.8559, -30.8885), (0.1034, -0.0458, 0.9935)
-Vec3d inlet_half = Vec3d(2.0 * dp_0, 3.0 * scaling, 3.0 * scaling);
+Vec3d inlet_half = Vec3d(0.5 * thickness, 3.0 * scaling, 3.0 * scaling);
 Vec3d inlet_normal(-0.1034, 0.0458, -0.9935);
-Vec3d inlet_translation = Vec3d(1.5611, 5.8559, -30.8885) * scaling + inlet_normal * 2.0 * dp_0;
+Vec3d inlet_translation = Vec3d(1.5611, 5.8559, -30.8885) * scaling + inlet_normal * 0.5 * thickness;
 Vec3d inlet_standard_direction(1, 0, 0);
 RotationResult inlet_rotation_result = RotationCalculator(inlet_normal, inlet_standard_direction);
 Rotation3d inlet_rotation(inlet_rotation_result.angle, inlet_rotation_result.axis);
 
 // outlet1 R=1.9416, (-2.6975, -0.4330, 21.7855), (-0.3160, -0.0009, 0.9488)
-Vec3d outlet_01_half = Vec3d(2.0 * dp_0, 2.4 * scaling, 2.4 * scaling);
+Vec3d outlet_01_half = Vec3d(0.5 * thickness, 2.4 * scaling, 2.4 * scaling);
 Vec3d outlet_01_normal(-0.3160, -0.0009, 0.9488);
 Vec3d outlet_01_translation = Vec3d(-2.6975, -0.4330, 21.7855) * scaling + outlet_01_normal * 2.0 * dp_0;
 Vec3d outlet_01_standard_direction(1, 0, 0);
@@ -90,9 +92,9 @@ RotationResult outlet_01_rotation_result = RotationCalculator(outlet_01_normal, 
 Rotation3d outlet_01_rotation(outlet_01_rotation_result.angle, outlet_01_rotation_result.axis);
 
 // outlet2 R=1.3261, (9.0220, 0.9750, 18.6389), (-0.0399, 0.0693, 0.9972)
-Vec3d outlet_02_half = Vec3d(2.0 * dp_0, 2.0 * scaling, 2.0 * scaling);
+Vec3d outlet_02_half = Vec3d(0.5 * thickness, 2.0 * scaling, 2.0 * scaling);
 Vec3d outlet_02_normal(-0.0399, 0.0693, 0.9972);
-Vec3d outlet_02_translation = Vec3d(9.0220, 0.9750, 18.6389) * scaling + outlet_02_normal * 2.0 * dp_0;
+Vec3d.3261 outlet_02_translation = Vec3d(9.0220, 0.9750, 18.6389) * scaling + outlet_02_normal * 2.0 * dp_0;
 Vec3d outlet_02_standard_direction(1, 0, 0);
 RotationResult outlet_02_rotation_result = RotationCalculator(outlet_02_normal, outlet_02_standard_direction);
 Rotation3d outlet_02_rotation(outlet_02_rotation_result.angle, outlet_02_rotation_result.axis);
@@ -113,11 +115,11 @@ int main(int ac, char *av[])
     //	Creating body, materials and particles.
     //----------------------------------------------------------------------
     RealBody imported_model(sph_system, makeShared<SolidBodyFromMesh>("SolidBodyFromMesh"));
-    // level set shape is used for particle relaxation
-    imported_model.defineBodyLevelSetShape()->correctLevelSetSign()->writeLevelSet(sph_system);
+    imported_model.defineAdaptation<SPHAdaptation>(1.15, 1.0);
+    imported_model.defineBodyLevelSetShape(level_set_refinement_ratio)->correctLevelSetSign()->writeLevelSet(sph_system);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
-        ? imported_model.generateParticles<BaseParticles, Reload>(imported_model.getName())
-        : imported_model.generateParticles<BaseParticles, Lattice>();
+        ? imported_model.generateParticles<SurfaceParticles, Reload>(imported_model.getName())
+        : imported_model.generateParticles<SurfaceParticles, ThickSurface, Lattice>(thickness);
 
     RealBody test_body_in(
         sph_system, makeShared<AlignedBoxShape>(Transform(Rotation3d(inlet_rotation), Vec3d(inlet_translation)), inlet_half, "TestBodyIn"));
@@ -154,7 +156,8 @@ int main(int ac, char *av[])
         using namespace relax_dynamics;
         SimpleDynamics<RandomizeParticlePosition> random_imported_model_particles(imported_model);
         /** A  Physics relaxation step. */
-        RelaxationStepLevelSetCorrectionInner relaxation_step_inner(imported_model_inner);
+        ShellRelaxationStep relaxation_step_inner(imported_model_inner);
+        ShellNormalDirectionPrediction shell_normal_prediction(imported_model_inner, thickness);
 
         // here, need a class to switch particles in aligned box to ghost particles (not real particles)
         SimpleDynamics<AlignedBoxParticlesDetection> inlet_particles_detection(inlet_detection_box, xAxis);
@@ -169,7 +172,7 @@ int main(int ac, char *av[])
         //	Particle relaxation starts here.
         //----------------------------------------------------------------------
         random_imported_model_particles.exec(0.25);
-        relaxation_step_inner.SurfaceBounding().exec();
+        relaxation_step_inner.MidSurfaceBounding().exec();
         write_imported_model_to_vtp.writeToFile(0.0);
         imported_model.updateCellLinkedList();
         // write_cell_linked_list.writeToFile(0.0);
@@ -193,6 +196,7 @@ int main(int ac, char *av[])
         outlet01_particles_detection.exec();
         outlet02_particles_detection.exec();
 
+        shell_normal_prediction.exec();
         write_imported_model_to_vtp.writeToFile(ite_p);
         write_particle_reload_files.writeToFile(0);
 
