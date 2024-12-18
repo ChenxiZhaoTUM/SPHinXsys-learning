ed5c55a9acabb6e980f6bffcd1294eba3d0a577f
Chenxi Zhao <chenxi.zhao@tum.de>
2024-12-18 13:08:12 +0100

    carotid: carotid vel inlet + 0Pa outlet

--- tests/extra_source_and_tests/case_3d_carotid_VIPO_rigid_shell/case_3d_carotid_VIPO_rigid_shell.cpp
+++ tests/extra_source_and_tests/case_3d_carotid_VIPO_rigid_shell/case_3d_carotid_VIPO_rigid_shell.cpp
@@ -59,6 +59,160 @@ public:
     }
 };
 //----------------------------------------------------------------------
+//	Shell particle generation.
+//----------------------------------------------------------------------
+class FromSTLFile;
+template <>
+class ParticleGenerator<SurfaceParticles, FromSTLFile> : public ParticleGenerator<SurfaceParticles>
+{
+    Real mesh_total_area_;
+    Real particle_spacing_;
+    const Real thickness_;
+    Real avg_particle_volume_;
+    size_t planned_number_of_particles_;
+
+    TriangleMeshShapeSTL* mesh_shape_;
+    Shape &initial_shape_;
+
+public:
+    explicit ParticleGenerator(SPHBody &sph_body, SurfaceParticles &surface_particles, TriangleMeshShapeSTL* mesh_shape) 
+        : ParticleGenerator<SurfaceParticles>(sph_body, surface_particles),
+        mesh_total_area_(0),
+        particle_spacing_(sph_body.sph_adaptation_->ReferenceSpacing()),
+        thickness_(particle_spacing_),
+        avg_particle_volume_(pow(particle_spacing_, Dimensions - 1) * thickness_),
+        planned_number_of_particles_(0),
+        mesh_shape_(mesh_shape), initial_shape_(sph_body.getInitialShape()) 
+    {
+        if (!mesh_shape_)
+        {
+            std::cerr << "Error: Mesh shape is not set!" << std::endl;
+            return;
+        }
+
+        if (!initial_shape_.isValid())
+        {
+            std::cout << "\n BaseParticleGeneratorLattice Error: initial_shape_ is invalid." << std::endl;
+            std::cout << __FILE__ << ':' << __LINE__ << std::endl;
+            throw;
+        }
+    }
+
+    virtual void prepareGeometricData() override
+    {
+        
+        // Preload vertex positions
+        std::vector<std::array<Real, 3>> vertex_positions;
+        int num_vertices = mesh_shape_->getTriangleMesh()->getNumVertices();
+        vertex_positions.reserve(num_vertices);
+        for (int i = 0; i < num_vertices; i++)
+        {
+            const auto &p = mesh_shape_->getTriangleMesh()->getVertexPosition(i);
+            vertex_positions.push_back({Real(p[0]), Real(p[1]), Real(p[2])});
+        }
+
+        // Preload face
+        std::vector<std::array<int, 3>> faces;
+        int num_faces = mesh_shape_->getTriangleMesh()->getNumFaces();
+        std::cout << "num_faces calculation = " << num_faces << std::endl;
+        faces.reserve(num_faces);
+        for (int i = 0; i < num_faces; i++)
+        {
+            auto f1 = mesh_shape_->getTriangleMesh()->getFaceVertex(i, 0);
+            auto f2 = mesh_shape_->getTriangleMesh()->getFaceVertex(i, 1);
+            auto f3 = mesh_shape_->getTriangleMesh()->getFaceVertex(i, 2);
+            faces.push_back({f1, f2, f3});
+        }
+
+        // Calculate total volume
+        std::vector<Real> face_areas(num_faces);
+        for (int i = 0; i < num_faces; ++i)
+        {
+            Vec3d vertices[3];
+            for (int j = 0; j < 3; ++j)
+            {
+                const auto& pos = vertex_positions[faces[i][j]];
+                vertices[j] = Vec3d(pos[0], pos[1], pos[2]);
+            }
+
+            Real each_area = calculateEachFaceArea(vertices);
+            face_areas[i] = each_area;
+            mesh_total_area_ += each_area;
+        }
+
+        Real number_of_particles = mesh_total_area_ * thickness_ / avg_particle_volume_ + 0.5;
+        planned_number_of_particles_ = int(number_of_particles);
+        std::cout << "planned_number_of_particles calculation = " << planned_number_of_particles_ << std::endl;
+
+        // initialize a uniform distribution between 0 (inclusive) and 1 (exclusive)
+        std::mt19937_64 rng;
+        std::uniform_real_distribution<Real> unif(0, 1);
+
+        // Calculate the interval based on the number of particles.
+        Real interval = planned_number_of_particles_ / (num_faces + TinyReal);  // if planned_number_of_particles_ >= num_faces, every face will generate particles
+        if (interval <= 0)
+            interval = 1; // It has to be lager than 0.
+
+        for (int i = 0; i < num_faces; ++i)
+        {
+            Vec3d vertices[3];
+            for (int j = 0; j < 3; ++j)
+            {
+                const auto& pos = vertex_positions[faces[i][j]];
+                vertices[j] = Vec3d(pos[0], pos[1], pos[2]);
+            }
+
+            Real random_real = unif(rng);
+            if (random_real <= interval && base_particles_.TotalRealParticles() < planned_number_of_particles_)
+            {
+                // Generate particle at the center of this triangle face
+                // generateParticleAtFaceCenter(vertices);
+                
+                // Generate particles on this triangle face, unequal
+                int particles_per_face = std::max(1, int(planned_number_of_particles_ * (face_areas[i] / mesh_total_area_)));
+                generateParticlesOnFace(vertices, particles_per_face);
+            }
+        }
+
+        std::cout << "Shell particle generation finish!" << std::endl;
+    }
+
+private:
+    Real calculateEachFaceArea(const Vec3d vertices[3])
+    {
+        Vec3d edge1 = vertices[1] - vertices[0];
+        Vec3d edge2 = vertices[2] - vertices[0];
+        Real area = 0.5 * edge1.cross(edge2).norm();
+        return area;
+    }
+
+    void generateParticleAtFaceCenter(const Vec3d vertices[3])
+    {
+        Vec3d face_center = (vertices[0] + vertices[1] + vertices[2]) / 3.0;
+
+        addPositionAndVolumetricMeasure(face_center, avg_particle_volume_/thickness_);
+        addSurfaceProperties(initial_shape_.findNormalDirection(face_center), thickness_);
+    }
+
+    void generateParticlesOnFace(const Vec3d vertices[3], int particles_per_face)
+    {
+        for (int k = 0; k < particles_per_face; ++k)
+        {
+            Real u = static_cast<Real>(rand()) / static_cast<Real>(RAND_MAX);
+            Real v = static_cast<Real>(rand()) / static_cast<Real>(RAND_MAX);
+
+            if (u + v > 1.0) {
+                u = 1.0 - u;
+                v = 1.0 - v;
+            }
+            Vec3d particle_position = (1 - u - v) * vertices[0] + u * vertices[1] + v * vertices[2];
+
+            addPositionAndVolumetricMeasure(particle_position, avg_particle_volume_/thickness_);
+            addSurfaceProperties(initial_shape_.findNormalDirection(particle_position), thickness_);
+        }
+    }
+};
+//----------------------------------------------------------------------
 //	Buffer location.
 //----------------------------------------------------------------------
 struct RotationResult
@@ -130,7 +284,7 @@ Rotation3d outlet_down_emitter_rotation(outlet_down_rotation_result.angle + Pi, 
 //	Global parameters on the fluid properties
 //----------------------------------------------------------------------
 Real rho0_f = 1060; /**< Reference density of fluid. */
-Real U_f = 0.5;    /**< Characteristic velocity. */
+Real U_f = 2.0;    /**< Characteristic velocity. */
 /** Reference sound speed needs to consider the flow speed in the narrow channels. */
 Real c_f = 10.0 * U_f * SMAX(Real(1), DW_in * DW_in / (DW_out_up * DW_out_up + DW_out_down * DW_out_down));
 Real mu_f = 0.00355; /**< Dynamics viscosity. */
@@ -143,26 +297,97 @@ Real poisson = 0.49;               /** Poisson ratio. */
 //----------------------------------------------------------------------
 //	Inflow velocity
 //----------------------------------------------------------------------
+/** fluent vel */ 
+//struct InflowVelocity
+//{
+//    Real u_ref_, t_ref_, interval_;
+//    AlignedBoxShape &aligned_box_;
+//
+//    template <class BoundaryConditionType>
+//    InflowVelocity(BoundaryConditionType &boundary_condition)
+//        : u_ref_(0.1), t_ref_(0.218), interval_(0.5),
+//        aligned_box_(boundary_condition.getAlignedBox()){}
+//
+//    Vecd operator()(Vecd &position, Vecd &velocity, Real current_time)
+//    {
+//        Vecd target_velocity = velocity;
+//        int n = static_cast<int>(current_time / interval_);
+//        Real t_in_cycle = current_time - n * interval_;
+//
+//        target_velocity[0] = t_in_cycle < t_ref_ ? 0.5 * sin(4 * Pi * (current_time + 0.0160236)) : u_ref_;
+//        return target_velocity;
+//    }
+//};
+
+/** carotid vel */
 struct InflowVelocity
 {
-    Real u_ref_, t_ref_, interval_;
-    AlignedBoxShape &aligned_box_;
+    Real u_ave;
 
     template <class BoundaryConditionType>
     InflowVelocity(BoundaryConditionType &boundary_condition)
-        : u_ref_(0.1), t_ref_(0.218), interval_(0.5),
-        aligned_box_(boundary_condition.getAlignedBox()){}
+        : u_ave(0.0) {}
 
     Vecd operator()(Vecd &position, Vecd &velocity, Real current_time)
     {
         Vecd target_velocity = velocity;
-        int n = static_cast<int>(current_time / interval_);
-        Real t_in_cycle = current_time - n * interval_;
 
-        target_velocity[0] = t_in_cycle < t_ref_ ? 0.5 * sin(4 * Pi * (current_time + 0.0160236)) : u_ref_;
+        u_ave = 0.3782;
+        Real a[8] = {-0.1812, 0.1276, -0.08981, 0.04347, -0.05412, 0.02642, 0.008946, -0.009005};
+        Real b[8] = {-0.07725, 0.01466, 0.004295, -0.06679, 0.05679, -0.01878, 0.01869, -0.01888};
+        for (size_t i = 0; i < 8; i++)
+        {
+            u_ave = u_ave + a[i] * cos(8.302 * (i + 1) * current_time) + b[i] * sin(8.302 * (i + 1) * current_time);
+        }
+            
+        target_velocity[0] = u_ave;
+        target_velocity[1] = 0.0;
+        target_velocity[2] = 0.0;
+
         return target_velocity;
     }
 };
+
+/** aorta vel */
+//struct InflowVelocity
+//{
+//    Real u_ave, interval_;
+//
+//    template <class BoundaryConditionType>
+//    InflowVelocity(BoundaryConditionType &boundary_condition)
+//        : u_ave(0.0), interval_(0.66) {}
+//
+//    Vecd operator()(Vecd &position, Vecd &velocity, Real current_time)
+//    {
+//        Vecd target_velocity = velocity;
+//        int n = static_cast<int>(current_time / interval_);
+//        Real t_in_cycle = current_time - n * interval_;
+//
+//        u_ave = 5.0487;
+//        Real a[8] = {4.5287, -4.3509, -5.8551, -1.5063, 1.2800, 0.9012, 0.0855, -0.0480};
+//        Real b[8] = {-8.0420, -6.2637, 0.7465, 3.5239, 1.6283, -0.1306, -0.2738, -0.0449};
+//
+//        Real w = 2 * Pi / 1.0;
+//        for (size_t i = 0; i < 8; i++)
+//        {
+//            u_ave = u_ave + a[i] * cos(w * (i + 1) * t_in_cycle) + b[i] * sin(w * (i + 1) * t_in_cycle);
+//        }
+//
+//        //u_ave = fabs(u_ave);
+//            
+//        //target_velocity[0] = SMAX(2.0 * u_ave * (1.0 - (position[1] * position[1] + position[2] * position[2]) / radius_inlet / radius_inlet),
+//        //                          1.0e-2);
+//
+//        //target_velocity[0] = 2.0 * u_ave * (1.0 - (position[1] * position[1] + position[2] * position[2]) / radius_inlet / radius_inlet) * 5.9765 * scaling / 0.66;
+//
+//        target_velocity[0] = u_ave * (2.9293 * 2.9293 * 1.0E-2 * Pi / 5.9825);
+//        
+//        target_velocity[1] = 0.0;
+//        target_velocity[2] = 0.0;
+//
+//        return target_velocity;
+//    }
+//};
 //----------------------------------------------------------------------
 //	Pressure boundary definition.
 //----------------------------------------------------------------------
@@ -213,8 +438,8 @@ int main(int ac, char *av[])
     //	Build up the environment of a SPHSystem with global controls.
     //----------------------------------------------------------------------
     SPHSystem sph_system(system_domain_bounds, dp_0);
-    sph_system.setRunParticleRelaxation(true); // Tag for run particle relaxation for body-fitted distribution
-    sph_system.setReloadParticles(false);       // Tag for computation with save particles distribution
+    sph_system.setRunParticleRelaxation(false); // Tag for run particle relaxation for body-fitted distribution
+    sph_system.setReloadParticles(true);       // Tag for computation with save particles distribution
     sph_system.handleCommandlineOptions(ac, av)->setIOEnvironment();
     //----------------------------------------------------------------------
     //	Creating body, materials and particles.cd
